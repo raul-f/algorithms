dd2e893b7fbe0fd1371db6de3a708fbb66401a32
raul-f <raulf.dev@gmail.com>
2019-02-09 14:19:54 -0200

    Make basic/0002_reverse-a-string/app.js compliant with functional programming
    The names of the variables were not as clear as they could be and the original input was changed by the
    function. This was solved by renaming the variables and returning the 'output' variable instead of
    changing the string.
    
    Signed-off-by: raul-f <raulf.dev@gmail.com>

--- basic/0002_reverse-a-string/app.js
+++ basic/0002_reverse-a-string/app.js
@@ -1,16 +1,16 @@
 module.exports = {
-    reverseString: function(str) {
-        let firstStep = []
-        for (let i = str.length - 1; i >= 0; i--) {
-            firstStep.push(str[i])
-        }
-        //console.log(JSON.stringify(firstStep));
-        let secondStep = ""
-        for (let j = 0; j < firstStep.length; j++) {
-            secondStep += firstStep[j]
-        }
-        //console.log(secondStep);
-        str = secondStep
-        return str
-    }
+	reverseString: function(str) {
+		let inverseLetterArray = []
+		for (let i = str.length - 1; i >= 0; i--) {
+			inverseLetterArray.push(str[i])
+		}
+		//console.log(JSON.stringify(firstStep));
+		let output = ''
+		for (let j = 0; j < inverseLetterArray.length; j++) {
+			output += inverseLetterArray[j]
+		}
+		//console.log(secondStep);
+		// str = secondStep
+		return output
+	},
 }
